d0e8160284ab01be7b5dce6260c9fd6b3ad52738
Ben Verbraecken <ben.verbraecken@gmail.com>
2024-05-22 20:35:37 +0200

    Starting

---
+++ src/main/java/me/quartz/cnstaffchat/CNStaffchat.java
@@ -0,0 +1,17 @@
+package me.quartz.cnstaffchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CNStaffchat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
